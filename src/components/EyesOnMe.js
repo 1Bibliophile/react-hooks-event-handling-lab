25bad88cecf5424ba4564fd113d529367d11b84a
David Morgenstern <DavidtheConfident@gmail.com>
2021-12-12 22:11:23 -0600

    commit tree 5a65dd5bf7180af5b5f9a032530c13d5872c3766

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    
+    return(
+        <div>
+            <button input onFocus={(e) => console.log('Good!')}
+                    input onBlur={(e) => console.log('Hey! Eyes on me!')}>Eyes on me
+            </button>
+            {/*<button onBlur={(e) => console.log('Hey! Eyes on me!')}></button>*/}
+        </div>
+    )
+}
+
+export default EyesOnMe;
